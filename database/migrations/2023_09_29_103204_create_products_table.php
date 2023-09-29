6bcc8904a2394853e7ccf8ee6a64240d79f98c15
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-09-29 14:10:49 +0000

    Update 2023_09_29_103204_create_products_table.php

--- database/migrations/2023_09_29_103204_create_products_table.php
+++ database/migrations/2023_09_29_103204_create_products_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('brand_id')->constrained('brands')->cascadeOnDelete();
             $table->string('name');
+            $table->foreignId('brand_id')->constrained('brands')->cascadeOnDelete();
             $table->string('slug')->unique();
             $table->string('sku')->unique();
             $table->string('image');
